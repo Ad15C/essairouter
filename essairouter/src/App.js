beaf6b98a75a31b8187e48233101531c356273b2
Ad15C <ad15canon@hotmail.com>
2024-07-12 23:24:16 +0200

    nav

--- essairouter/src/App.js
+++ essairouter/src/App.js
@@ -1,13 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Time from './pages/Time';
+import { Link } from 'react-router-dom';
+
 
 function App() {
   return (
     <div className="App">
       <nav>
-        
+        <Link to='/'>Accueil</Link>
+        <Link to='/About'>A Propos</Link>
+        <Link to='/Contact'>Contact</Link>
+        <Link to='/Time'>Heure</Link>
       </nav>
-    </div>
+
+      <Routes>
+      <Route path='/' element={<Home />}></Route>
+      <Route path='/About' element={<About />}></Route>
+      <Route path='/Contact' element={<Contact />}></Route>
+      <Route path='/Time' element={<Time />}></Route>
+    </Routes>
+      
+   </div>
   );
 }
 
