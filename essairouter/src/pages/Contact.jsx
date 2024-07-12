b38cc8e985fe474f3a8ba3522ea307e578e58605
Ad15C <ad15canon@hotmail.com>
2024-07-12 22:53:21 +0200

    creationpages

---
+++ essairouter/src/pages/Contact.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Contact = () => {
+    return (
+        <main className="container">
+            <h1>Pour plus d'informations</h1>
+            <h2>Vous pourrez nous joindre via:</h2>
+                <ul>
+                    <li>Adresse</li>
+                    <li>Téléphone</li>
+                    <li>Mail</li>
+                </ul>
+        </main>
+    );
+}
+
+export default Contact;
